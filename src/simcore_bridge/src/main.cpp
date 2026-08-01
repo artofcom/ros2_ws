b2e05b43a6d371f5fe2ff93f74d39fc6f22b600c
Hyungtae <artofcom2@gmail.com>
2026-08-01 13:40:24 -0500

    Adding RViz support.

--- src/simcore_bridge/src/main.cpp
+++ src/simcore_bridge/src/main.cpp
@@ -9,6 +9,7 @@
 #include "simcore/Goal.h"
 #include "geometry_msgs/msg/pose2_d.hpp"
 #include "geometry_msgs/msg/point.hpp"
+#include "visualization_msgs/msg/marker.hpp"
 
 using namespace std::chrono_literals;
 
@@ -21,10 +22,13 @@ public:
         simulation_.Initialize();
         posePublisher_ = create_publisher<geometry_msgs::msg::Pose2D>("robot_pose", 10);
         goalSubscriber_ = create_subscription<geometry_msgs::msg::Point>( "goal", 10,
-        std::bind(&SimCoreNode::OnGoalReceived,
+                  std::bind(&SimCoreNode::OnGoalReceived,
                   this,
                   std::placeholders::_1));
 
+        robotMarkerPublisher_ = create_publisher<visualization_msgs::msg::Marker>( "robot_marker", 10);
+        goalMarkerPublisher_ = create_publisher<visualization_msgs::msg::Marker>( "goal_marker", 10);
+        
         timer_ = create_wall_timer(
             100ms,
             std::bind(&SimCoreNode::OnTimer, this));
@@ -41,25 +45,86 @@ private:
         simulation_.Update(0.1f);
 
         auto& world = simulation_.GetWorld();
-
         if (world.GetRobotCount() == 0)
-        {
             return;
-        }
-
+        
         auto robot = world.GetRobot(0);
-
         auto pose = robot->GetPose();
 
         geometry_msgs::msg::Pose2D msg;
-
         msg.x = pose.x;
         msg.y = pose.y;
         msg.theta = pose.theta;
-
         posePublisher_->publish(msg);
+
+        publishMarker(pose);
+        publishGoalMarker();
     }
     
+    void publishMarker(simcore::Pose2D& pose)
+    {
+        visualization_msgs::msg::Marker marker;
+
+        marker.header.frame_id = "map";
+        marker.header.stamp = now();
+        marker.ns = "robot";
+        marker.id = 0;
+
+        marker.type = visualization_msgs::msg::Marker::SPHERE;
+        marker.action = visualization_msgs::msg::Marker::ADD;
+
+        marker.pose.position.x = pose.x;
+        marker.pose.position.y = pose.y;
+        marker.pose.position.z = 0.0;
+
+        marker.pose.orientation.w = 1.0;
+
+        marker.scale.x = 0.5;
+        marker.scale.y = 0.5;
+        marker.scale.z = 0.5;
+
+        marker.color.a = 1.0;
+        marker.color.r = 0.2;
+        marker.color.g = 0.8;
+        marker.color.b = 0.2;
+
+        robotMarkerPublisher_->publish(marker);
+    }
+
+    void publishGoalMarker()
+    {
+        if (!hasGoal_)
+            return;
+
+        visualization_msgs::msg::Marker marker;
+
+        marker.header.frame_id = "map";
+        marker.header.stamp = now();
+
+        marker.ns = "goal";
+        marker.id = 0;
+
+        marker.type = visualization_msgs::msg::Marker::CYLINDER;
+        marker.action = visualization_msgs::msg::Marker::ADD;
+
+        marker.pose.position.x = currentGoal_.x;
+        marker.pose.position.y = currentGoal_.y;
+        marker.pose.position.z = 0.0;
+
+        marker.pose.orientation.w = 1.0;
+
+        marker.scale.x = 0.4;
+        marker.scale.y = 0.4;
+        marker.scale.z = 0.1;
+
+        marker.color.a = 1.0;
+        marker.color.r = 1.0;
+        marker.color.g = 0.1;
+        marker.color.b = 0.1;
+
+        goalMarkerPublisher_->publish(marker);
+}
+
     void OnGoalReceived(const geometry_msgs::msg::Point::SharedPtr msg)
     {
         auto goal = std::make_shared<simcore::Goal>();
@@ -67,6 +132,9 @@ private:
         goal->SetPosition(
             static_cast<float>(msg->x),
             static_cast<float>(msg->y));
+
+        currentGoal_ = goal->GetPose();
+        hasGoal_ = true;
 
         simulation_.GetWorld().SetGoal(goal);
 
@@ -83,6 +151,11 @@ private:
     simcore::Simulation simulation_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr goalSubscriber_;
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr robotMarkerPublisher_;
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr goalMarkerPublisher_;
+
+    simcore::Pose2D currentGoal_;
+    bool hasGoal_ = false;
 };
 
 int main(int argc, char* argv[])
