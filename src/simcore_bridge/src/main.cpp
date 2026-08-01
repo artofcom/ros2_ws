4f56fd30daa1c8ffa6b3573e6afc10c22fbb2c87
Hyungtae <artofcom2@gmail.com>
2026-08-01 12:53:56 -0500

    Add ROS2 pub/sub integration
    - Add simulation timer node
    - Publish robot pose
    - Subscribe goal topic
    - Connect ROS2 topics with SimCore

--- src/simcore_bridge/src/main.cpp
+++ src/simcore_bridge/src/main.cpp
@@ -3,6 +3,12 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "simcore/Simulation.h"
+#include "simcore/World.h"
+#include "simcore/Robot.h"
+#include "simcore/Pose2D.h"
+#include "simcore/Goal.h"
+#include "geometry_msgs/msg/pose2_d.hpp"
+#include "geometry_msgs/msg/point.hpp"
 
 using namespace std::chrono_literals;
 
@@ -13,6 +19,11 @@ public:
         : Node("simcore_bridge")
     {
         simulation_.Initialize();
+        posePublisher_ = create_publisher<geometry_msgs::msg::Pose2D>("robot_pose", 10);
+        goalSubscriber_ = create_subscription<geometry_msgs::msg::Point>( "goal", 10,
+        std::bind(&SimCoreNode::OnGoalReceived,
+                  this,
+                  std::placeholders::_1));
 
         timer_ = create_wall_timer(
             100ms,
@@ -28,11 +39,50 @@ private:
     void OnTimer()
     {
         simulation_.Update(0.1f);
+
+        auto& world = simulation_.GetWorld();
+
+        if (world.GetRobotCount() == 0)
+        {
+            return;
+        }
+
+        auto robot = world.GetRobot(0);
+
+        auto pose = robot->GetPose();
+
+        geometry_msgs::msg::Pose2D msg;
+
+        msg.x = pose.x;
+        msg.y = pose.y;
+        msg.theta = pose.theta;
+
+        posePublisher_->publish(msg);
     }
+    
+    void OnGoalReceived(const geometry_msgs::msg::Point::SharedPtr msg)
+    {
+        auto goal = std::make_shared<simcore::Goal>();
+
+        goal->SetPosition(
+            static_cast<float>(msg->x),
+            static_cast<float>(msg->y));
+
+        simulation_.GetWorld().SetGoal(goal);
+
+        RCLCPP_INFO(
+            get_logger(),
+            "New Goal: (%.2f, %.2f)",
+            msg->x,
+            msg->y);
+    }
+
+    rclcpp::Publisher<geometry_msgs::msg::Pose2D>::SharedPtr posePublisher_;
 
 private:
     simcore::Simulation simulation_;
     rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr goalSubscriber_;
 };
 
 int main(int argc, char* argv[])
